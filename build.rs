34d5f4a633263fce6dcaac85dbab8f705ad9ba5e
Mark Fisher <mark.j.fisher@gmail.com>
2024-07-20 15:59:18 +0100

    Rework features to separate xtensa and chip from default

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
+// This script is compiled and run before the build phase happens.
 fn main() {
     embuild::espidf::sysenv::output();
+
+    // DEBUG: output the current target_arch
+    // let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_else(|_| "unknown".to_string());
+    // println!("cargo:warning=Compiling for target architecture: {}", target_arch);
 }
--- build.sh
+++ build.sh
@@ -13,27 +13,35 @@ SHOW_MONITOR=0
 RELEASE_STRING=""
 TARGET="debug"
 
+FEATURES="--features "
+TARGET_ARCH="xtensa"
+TARGET_CHIP="esp32s3"
+
 function show_help {
   echo "Usage: $(basename $0) [options]"
   echo " -b          # build project"
   echo " -c          # clean project"
   echo " -m          # monitor device"
-  echo " -r          # release build (default debug)"
   echo " -u          # upload (flash) device"
+  echo ""
+  echo " -r          # release build, default = ${TARGET}"
+  echo " -a <arch>   # arch, default = ${TARGET_ARCH}"
+  echo " -p <chip>   # chipset, default = ${TARGET_CHIP}"
   exit 1
 }
-
 
 if [ $# -eq 0 ] ; then
   show_help
 fi
 
-while getopts "bchmru" flag
+while getopts "a:bchmp:ru" flag
 do
   case "$flag" in
+    a) TARGET_ARCH=${OPTARG} ;;
     b) DO_BUILD=1 ;;
     c) DO_CLEAN=1 ;;
     m) SHOW_MONITOR=1 ;;
+    p) TARGET_CHIP=${OPTARG} ;;
     r) IS_RELEASE=1 ;;
     u) DO_FLASH=1 ;;
     h) show_help ;;
@@ -41,6 +49,8 @@ do
   esac
 done
 shift $((OPTIND - 1))
+
+FEATURES="${FEATURES} ${TARGET_ARCH},${TARGET_CHIP}"
 
 if [ $IS_RELEASE -eq 1 ] ; then
   RELEASE_STRING="--release"
@@ -52,7 +62,7 @@ if [ $DO_CLEAN -eq 1 ] ; then
 fi
 
 if [ $DO_BUILD -eq 1 ] ; then
-  cargo build ${RELEASE_STRING}
+  cargo build ${RELEASE_STRING} ${FEATURES}
   if [ $? -ne 0 ] ; then
     echo "Failed to build, exiting"
     exit 1
@@ -61,7 +71,8 @@ if [ $DO_BUILD -eq 1 ] ; then
 fi
 
 if [ $DO_FLASH -eq 1 ] ; then
-  cargo espflash flash
+  # don't forget to add features list, this calls "cargo build" under the hood
+  cargo espflash flash ${FEATURES}
 fi
 
 if [ $SHOW_MONITOR -eq 1 ] ; then
