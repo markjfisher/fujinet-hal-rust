1b68ce5534b0b07c9dc4cf1f61a1e92f0de5f46e
Mark Fisher <mark.j.fisher@gmail.com>
2024-07-20 18:43:14 +0100

    Just 1 flash, 5ms startup delay

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,17 @@ fn main() {
     platform::init_platform();
     logger::init().expect("Failed to initialize the logger.");
 
-    // blinky test!
-    let mut led_level: u32 = 0;
-    let mut delay: u32 = 20;
-    let mut factor: f64 = 1.05;
-    unsafe {
-        gpio_reset_pin(gpio_num_t_GPIO_NUM_38);
-        gpio_set_direction(gpio_num_t_GPIO_NUM_38, gpio_mode_t_GPIO_MODE_OUTPUT);
-    }
-
     log::info!("info channel");
     log::warn!("watch out kids it's working! {}", "foo!");
     log::error!("gremlins at work");
     log::debug!("get that debug printed!");
     log::trace!("get that trace printed!");
 
-    loop {
-        unsafe { gpio_set_level(gpio_num_t_GPIO_NUM_38 as i32, led_level); }
-        led_level = 1 - led_level;
-        FreeRtos::delay_ms(delay);
-        delay = (delay as f64 * factor).round() as u32;
-        if delay > 400 {
-            factor = 0.95238;
-        }
-        if delay <= 20 {
-            factor = 1.05;
-        }
+    unsafe {
+        gpio_reset_pin(gpio_num_t_GPIO_NUM_38);
+        gpio_set_direction(gpio_num_t_GPIO_NUM_38, gpio_mode_t_GPIO_MODE_OUTPUT);
+        gpio_set_level(gpio_num_t_GPIO_NUM_38 as i32, 0);
+        FreeRtos::delay_ms(1000);
+        gpio_set_level(gpio_num_t_GPIO_NUM_38 as i32, 1);
     }
 }
